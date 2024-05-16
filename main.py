61913857504727d07b84e782d35f3a942acf9c94
Jimmy <49186603+JimbossJxy@users.noreply.github.com>
2024-05-16 12:02:34 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Author Block
+Author: James Collum
+Date Creation: 16/05/2024
+Document Name: main.py
+Purpose of Document: Acting as the main file to run the majority of the code from
+Referenced Code:
+
+
+"""
