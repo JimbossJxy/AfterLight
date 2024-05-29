a44175c61f98f0cc90ae93575b6623110bd8ab06
Jimmy <49186603+JimbossJxy@users.noreply.github.com>
2024-05-30 00:28:26 +1000

    Changes to inventory.py and added global variables

--- src/gameFunctions/inventory.py
+++ src/gameFunctions/inventory.py
@@ -9,3 +9,36 @@ Referenced Code:
 
 
 """
+
+import configparser
+import logging
+import util
+import os
+import pathlib
+import pickle
+import sys
+import shutil
+from util.misc import misc
+from pathlib import Path
+from tempfile import TemporaryDirectory, mkdtemp
+from logging.handlers import RotatingFileHandler
+
+class inventory:
+    def __init__(self):
+        self.inventory = {}
+        self.config = configparser.ConfigParser()
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.handler = RotatingFileHandler(str(Path.home() / "Documents" / "Afterlight" / "Logs" / "inventory.log"), maxBytes=100000, backupCount=5)
+        self.logger.addHandler(self.handler)
+        self.misc = misc()
+        self.warningPopup = self.misc.warningPopup
+        self.errorPopup = self.misc.errorPopup
+        self.defaultPath = str(Path.home() / "Documents" / "Afterlight")
+        self.savePath = str(Path.home() / "Documents" / "Afterlight" / "Saves")
+    
+    def loadInventory(self):
+        """
+        Loads the players inventory from a .dat file
+        """
+        
---
+++ src/variables.py
@@ -0,0 +1,253 @@
+"""
+Author Block
+Author: James Collum
+Date Creation: 29/05/2024
+Document Name: variables.py
+Purpose of Document: This document will be used to store all global variables for the game.
+
+"""
+
+inventory = [
+    {
+        "hotbar": {
+            "position1": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                "isEquipped": True,
+                'maxQuantity': 1
+            },
+            "position2": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                "isEquipped": False,
+                'maxQuantity': 1
+            },
+            "position3": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                "isEquipped": False,
+                'maxQuantity': 1
+            },
+            "position4": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                "isEquipped": False,
+                'maxQuantity': 1
+            },
+            "position5": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                "isEquipped": False,
+                'maxQuantity': 1
+            },
+            "position6": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                "isEquipped": False,
+                'maxQuantity': 1
+            },
+            "position7": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                "isEquipped": False,
+                'maxQuantity': 1
+            }
+        },
+        "row1": {
+            "position1": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position2": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position3": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position4": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position5": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position6": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position7": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            }
+            
+        },
+        "row2": {
+            "position1": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position2": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position3": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position4": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position5": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position6": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position7": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            }
+        },
+        "row3": {
+            "position1": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position2": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position3": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position4": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position5": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position6": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            },
+            "position7": {
+                "item": "empty",
+                "quantity": 0,
+                "description": "",
+                "isStackable": False,
+                "isUsable": False,
+                'maxQuantity': 1
+            }
+        }
+    }
+]
